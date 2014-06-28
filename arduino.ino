1114c5b6bcc57bfd295a7d556f0747fd7f5613a8
Tobias Wilken <tooangel@tooangel.de>
2014-06-28 19:51:56 +0200

    arduino

---
+++ arduino.ino
@@ -0,0 +1,19 @@
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Hello world!");  // prints hello with ending line break 
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);    
+  //Serial.print("VG400: ");
+  Serial.println(sensorValue);
+
+  
+  delay(1000);  
+}
+
