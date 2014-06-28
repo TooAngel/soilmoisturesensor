f7735d95574951be672a823bf55c7cefbbd97ac2
Tobias Wilken <tooangel@tooangel.de>
2014-06-28 23:12:28 +0200

    Post request

--- run.py
+++ run.py
@@ -12,10 +12,11 @@ def main():
         ser = serial.Serial('/dev/ttyACM1', 9600)
     
     while 1 :
+        host = '192.168.1.173:5000'
         measure = ser.readline()
         date = time.time()
         data = {'data': date, 'measure': measure}
-        print requests.post('http://192.168.0.101:5000/points/', headers={'content-type': 'application/json'}, data=json.dumps(data))
+        print requests.post('http://{}/points/'.format(host), headers={'content-type': 'application/json'}, data=json.dumps(data))
 
 if __name__ == '__main__':
     main()
