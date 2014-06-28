20bd24569856e9ef93d00f2eb858a9abb4568d94
Tobias Wilken <tooangel@tooangel.de>
2014-06-29 01:44:18 +0200

    Add ino.ini

---
+++ .build/leonardo/src/arduino.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+void setup();
+void loop();
+#line 1 "src/arduino.ino"
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Hello world!");  // prints hello with ending line break 
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);    
+  Serial.println(sensorValue);
+  
+  delay(60000);  
+}
+
