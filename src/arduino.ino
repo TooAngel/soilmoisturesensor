37fdd78c8303c311a9f6368c9d85f212b4fef90d
Tobias Wilken <tooangel@tooangel.de>
2014-07-01 21:26:41 +0200

    RedFly

--- src/arduino.ino
+++ src/arduino.ino
@@ -1,10 +1,20 @@
+#include <RedFly.h>
+#include <RedFlyClient.h>
 
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Hello world!");  // prints hello with ending line break 
+  Serial.println("Hello world!");  // prints hello with ending line break
+  
+  ret = RedFly.init();
+  Serial.println('init');
+  Serial.println(ret);
+  RedFly.scan();
+  ret = RedFly.join("TooWlan", "Man lebt nur einmal", INFRASTRUCTURE);
+  Serial.println('join');
+  Serial.println(ret);
 }
 
 void loop() {
