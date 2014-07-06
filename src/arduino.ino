6779b5c19c49154223880e9a4c57aae8f57c334e
Tobias Wilken <tooangel@tooangel.de>
2014-07-06 21:54:10 +0200

    WLAN debugging

--- src/arduino.ino
+++ src/arduino.ino
@@ -9,21 +9,22 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Arduino starting");
   
-  delay(10000);
-  ret = RedFly.init();
-  Serial.print("RedFly.init: ");
-  Serial.println(ret);
-//  RedFly.scan();
-//  ret = RedFly.join("TooWlan", "Man lebt nur einmal", INFRASTRUCTURE);
-//  Serial.print("RedFly.join: ");
-//  Serial.println(ret);
 }
 
 void loop() {
   // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);    
-  Serial.println(sensorValue);
+//  sensorValue = analogRead(sensorPin);    
+//  Serial.println(sensorValue);
   
-  delay(1000);
+//  delay(1000);
+  delay(10000);
+  ret = RedFly.init();
+  Serial.print("RedFly.init: ");
+  Serial.println(ret);
+  RedFly.scan();
+  ret = RedFly.join("TooWlan", "Man lebt nur einmal", INFRASTRUCTURE);
+  Serial.print("RedFly.join: ");
+  Serial.println(ret);
+  for(;;){}
 }
 
