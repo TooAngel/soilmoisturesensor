2aca0b17f5dd7acf326d045d2f0a1c1ee65a0884
Tobias Wilken <tooangel@tooangel.de>
2015-06-07 21:29:19 +0200

    Add parse_char

--- lib/HttpLibrary/HttpLibrary.cpp
+++ lib/HttpLibrary/HttpLibrary.cpp
@@ -6,25 +6,25 @@
 #include <Arduino.h>
 #include <Logging.h>
 
-//unsigned int dataPos = 0;
-//int return_int = 0;
+// unsigned int dataPos = 0;
+// int return_int = 0;
 //
-//char c = '\0';
-//ParseState parseState = ReadProtocol;
+// char c = '\0';
+// ParseState parseState = ReadProtocol;
 //
-//unsigned int protocolPos = 0;
-//char protocol[10];
-//const char* protocolPrefix = "HTTP/*.* ";
-//const char* protocolPtr = protocolPrefix;
-//unsigned int statusMessagePos = 0;
-//char statusMessage[20];
-//int statusCode = 0;
-//char headerline[100];
-//int headerlinePos = 0;
-//int contentLength = 0;
+// unsigned int protocolPos = 0;
+// char protocol[10];
+// const char* protocolPrefix = "HTTP/*.* ";
+// const char* protocolPtr = protocolPrefix;
+// unsigned int statusMessagePos = 0;
+// char statusMessage[20];
+// int statusCode = 0;
+// char headerline[100];
+// int headerlinePos = 0;
+// int contentLength = 0;
 //
 //
-//void readProtocol() {
+// void readProtocol() {
 //    if ((*protocolPtr == '*') || (*protocolPtr == c)) {
 //        protocol[protocolPos++] = c;
 //        protocolPtr++;
@@ -34,9 +34,9 @@
 //            Log.Verbose("Protocol %d"CR, protocol);
 //        }
 //    }
-//}
+// }
 //
-//int readStatusCode() {
+// int readStatusCode() {
 //    if (isdigit(c)) {
 //        statusCode = statusCode * 10 + (c - '0');
 //    } else {
@@ -48,9 +48,9 @@
 //        Log.Info("Status %d"CR, statusCode);
 //    }
 //    return 0;
-//}
+// }
 //
-//void readStatusMessage() {
+// void readStatusMessage() {
 //    if (c == '\n') {
 //        statusMessage[statusMessagePos++] = '\0';
 //        Log.Verbose("Message %s"CR, statusMessage);
@@ -58,9 +58,9 @@
 //        return;
 //    }
 //    statusMessage[statusMessagePos++] = c;
-//}
+// }
 //
-//void readHeader() {
+// void readHeader() {
 //    if (c == '\n') {
 //        if (headerlinePos == 1) {
 //            Log.Debug("Empty line found - finishing headers"CR);
@@ -74,15 +74,15 @@
 //    }
 //
 //    headerline[headerlinePos++] = c;
-//}
+// }
 //
-//int readData() {
+// int readData() {
 //    Log.Info("ReadData %c"CR, c);
 //    return_int = c - '0';
 //    parseState = Done;
-//}
+// }
 //
-//int receive_data(RedFlyClient client) {
+// int receive_data(RedFlyClient client) {
 //    dataPos = 0;
 //    return_int = 0;
 //    c = '\0';
@@ -125,8 +125,12 @@
 //        }
 //    } while (c != -1);
 //    return return_int;
-//}
+// }
 //
+
+int parse_char(char c) {
+//    Log.Debug("response: %s"CR, c);
+}
 
 int receive_data(RedFlyClient client) {
     int a = 0;
@@ -137,6 +141,9 @@ int receive_data(RedFlyClient client) {
 
     do {
         c = client.read();
+
+        parse_char(c);
+
         if ((c != -1) && (len < (sizeof(data) - 1))) {
             data[len++] = c;
         }
